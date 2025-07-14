abcbdecf4bf2442d7da9ed855cc56ea13d44d63c
waino <83163000@gmail.com>
2025-07-14 20:38:50 +0800

    feat✨: 更新应用标题和PWA元数据，优化移动端图标配置

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,13 +25,13 @@ function MyApp({ Component, pageProps }: NextAppProps) {
         <meta name="format-detection" content="telephone=no" />
         
         {/* 页面标题 */}
-        <title>SSG销售助手 - 您的智能销售伙伴</title>
+        <title>销售助手</title>
         
         {/* PWA Meta Tags */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="SSG销售助手" />
         <meta name="application-name" content="SSG销售助手" />
         <meta name="theme-color" content="#1f2937" />
@@ -43,17 +43,22 @@ function MyApp({ Component, pageProps }: NextAppProps) {
         <meta name="keywords" content="销售助手,数据分析,销售管理,PWA应用,SSG" />
         <meta name="author" content="SSG Sales Team" />
         
-        {/* 单图标配置 - 全部指向ssg.png */}
+        {/* 图标配置 - 移动端优化 */}
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/icons/ssg.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/ssg.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/ssg.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/ssg.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/ssg.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/ssg.png" />
+        
+        {/* Android Chrome */}
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-status-bar-style" content="black" />
         
         {/* Microsoft Tiles */}
         <meta name="msapplication-TileImage" content="/icons/ssg.png" />
+        <meta name="msapplication-TileColor" content="#1f2937" />
         
-        {/* Prevent zoom on input focus (iOS) */}
+        {/* 禁用iOS输入框放大 */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
       <AuthProvider>
